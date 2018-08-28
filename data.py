ab70594408c4c99c2107d2cf45a7e136e741ae9b
Celso Ernani <celsoernane@gmail.com>
2018-08-28 11:27:51 -0300

    Study for Alice

---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    Articles = [
+        {
+            'id': 1,
+            'title': 'Aula 01',
+            'body': 'A aula 01 trata-se de...',
+            'author': 'Celso', 
+            'create_date': '04-05-1998'
+
+        },
+          {
+            'id': 2,
+            'title': 'Aula 02',
+            'body': 'A aula 02 trata-se de...',
+            'author': 'Ernani', 
+            'create_date': '01-05-1998'
+
+        },
+          {
+            'id': 3,
+            'title': 'Aula 03',
+            'body': 'A aula 03 trata-se de...',
+            'author': 'Iago', 
+            'create_date': '04-05-1998'
+
+        }
+    ]
+    return Articles
